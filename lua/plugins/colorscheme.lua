726c86e3e3b7596cc4994095933f3d7d2935f62a
iro <orisneh@gmail.com>
2024-04-27 20:45:39 +0300

    added colorscheme fallback

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,28 @@
-vim.cmd("colorscheme desert")
+local colorscheme = "carbonfox"
+local fallback_colorscheme = "desert"
 
 return {
-  "EdenEast/nightfox.nvim",
+
+  { "EdenEast/nightfox.nvim" } ,
+
+  { "xiyaowong/transparent.nvim", config = function()
+    vim.keymap.set("n", "<leader>tr", function()
+      vim.cmd("TransparentToggle")
+    end, { desc = "toggle background transparency" })
+  end},
+
+  {
+    "NvChad/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+
+      local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
+      if not status_ok then
+        print("colorscheme " .. colorscheme .. " not found, falling back to " .. fallback_colorscheme)
+        vim.cmd.colorscheme(fallback_colorscheme)
+      end
+    end,
+  },
+
 }
 
