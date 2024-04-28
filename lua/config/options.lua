019fa8e9aa86afec6ec7a524a83c42ce8b6e1f8a
iro <orisneh@gmail.com>
2024-04-28 17:12:00 +0300

    added tabular and new options function

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,44 +1,56 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-local opt = vim.opt
-
-opt.clipboard = "unnamedplus" -- Sync with system clipboard
-opt.completeopt = "menu,menuone,noselect"
-opt.confirm = true -- Confirm to save changes before exiting modified buffer
-opt.cursorline = true -- Enable highlighting of the current line
-opt.expandtab = true -- Use spaces instead of tabs
-opt.ignorecase = true -- Ignore case
-opt.inccommand = "split" -- preview incremental substitute
-opt.laststatus = 3 -- global statusline
-opt.mouse = "a" -- Enable mouse mode
-opt.number = true -- Print line number
-opt.relativenumber = true -- Relative line numbers
-opt.scrolloff = 4 -- Lines of context
-opt.shiftround = true -- Round indent
-opt.shiftwidth = 4 -- Size of an indent
-opt.sidescrolloff = 8 -- Columns of context
-opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
-opt.smartcase = true -- Don't ignore case with capitals
-opt.smartindent = true -- Insert indents automatically
-opt.splitbelow = true -- Put new windows below current
-opt.splitkeep = "screen"
-opt.tabstop = 4 -- Number of spaces tabs count for
-opt.termguicolors = true -- True color support
-opt.undofile = true
-opt.undolevels = 10000
-opt.updatetime = 200 -- Save swap file and trigger CursorHold
-opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
-opt.wildmode = "longest:full,full" -- Command-line completion mode
-opt.winminwidth = 5 -- Minimum window width
-opt.wrap = false -- Disable line wrap
-opt.colorcolumn = "0" -- disable color colum
-opt.fillchars = { eob = " " } -- remove the "~" char from end of buffer
-
-if vim.fn.has("nvim-0.10") == 1 then
-  opt.smoothscroll = true
+local set = function(opts, scope)
+  scope = scope or vim.opt
+  for k, v in pairs(opts) do
+    scope[k] = v
+  end
 end
 
--- Folding
-vim.opt.foldlevel = 99
+local global_opts = {
+  mapleader = " ",
+  maplocalleader = "\\",
+}
+
+local opts = {
+  clipboard = "unnamedplus", -- Sync with system clipboard
+  completeopt = "menu,menuone,noselect",
+  confirm = true, -- Confirm to save changes before exiting modified buffer
+  cursorline = true, -- Enable highlighting of the current line
+  expandtab = true, -- Use spaces instead of tabs
+  ignorecase = true, -- Ignore case
+  inccommand = "split", -- preview incremental substitute
+  laststatus = 3, -- global statusline
+  mouse = "a", -- Enable mouse mode
+  number = true, -- Print line number
+  relativenumber = true, -- Relative line numbers
+  scrolloff = 4, -- Lines of context
+  shiftround = true, -- Round indent
+  shiftwidth = 4, -- Size of an indent
+  sidescrolloff = 8, -- Columns of context
+  signcolumn = "yes", -- Always show the signcolumn, otherwise it would shift the text each time
+  smartcase = true, -- Don't ignore case with capitals
+  smartindent = true, -- Insert indents automatically
+  splitbelow = true, -- Put new windows below current
+  splitkeep = "screen",
+  tabstop = 4, -- Number of spaces tabs count for
+  termguicolors = true, -- True color support
+  undofile = true,
+  undolevels = 10000,
+  updatetime = 200, -- Save swap file and trigger CursorHold
+  virtualedit = "block", -- Allow cursor to move where there is no text in visual block mode
+  wildmode = "longest:full,full", -- Command-line completion mode
+  winminwidth = 5, -- Minimum window width
+  wrap = false, -- Disable line wrap
+  colorcolumn = "0", -- disable color colum
+  fillchars = { eob = " " }, -- remove the "~" char from end of buffer
+  foldlevel = 99,
+}
+
+if vim.fn.has("nvim-0.10") == 1 then
+  vim.list_extend(opts, {
+    smoothscroll = true,
+  })
+end
+
+set(global_opts, vim.g)
+set(opts)
 
