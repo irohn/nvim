c580231b20b4ae35b4dd51cad6cbfe4e8a54d838
iro <orisneh@gmail.com>
2024-05-05 17:48:00 +0300

    Removed background hilight group from default themes

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -1,5 +1,5 @@
 local colorscheme = "carbonfox"
-local fallback_colorscheme = "vim"
+local fallback_colorscheme = "retrobox"
 
 local status, _ = pcall(vim.cmd.colorscheme, colorscheme)
 if not status then
@@ -8,5 +8,6 @@ if not status then
         " falling back to " ..
         fallback_colorscheme)
   vim.cmd.colorscheme(fallback_colorscheme)
+  vim.cmd("hi Normal guibg=NONE ctermbg=NONE") -- remove background
 end
 
