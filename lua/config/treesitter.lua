ba1a1bae4ceb7a1f9bddd1c37dafd34f2e6b926e
Ori Sne <orisne@greeneye.ag>
2024-10-18 12:04:01 +0800

    Added treesitter config

---
+++ lua/config/treesitter.lua
@@ -0,0 +1,18 @@
+local configs = require("nvim-treesitter.configs")
+
+configs.setup({
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+  sync_install = false,
+  auto_install = true,
+  highlight = { enable = true },
+  indent = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<enter>", -- set to `false` to disable one of the mappings
+      node_incremental = "<enter>",
+      scope_incremental = false,
+      node_decremental = "<bs>",
+    },
+  },
+})
