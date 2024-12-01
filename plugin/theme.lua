d6399d186f0833ba3125d92153b3e18acc5a68e6
nixos <orisneh@gmail.com>
2024-12-01 20:35:40 +0000

    added telescope ui select

--- plugin/theme.lua
+++ plugin/theme.lua
@@ -4,13 +4,10 @@ local colorscheme_overwrite = nil
 
 local defaults = {
   preview = true,
-  keys = "<leader>th",
+  keymap = "<leader>th",
 }
 
-local builtin = require("telescope.builtin")
-vim.opt.termguicolors = true
-
-function M.save_colorscheme(color_name)
+local function save_colorscheme(color_name)
   local cache_dir = vim.fn.stdpath('cache')
   local file = io.open(cache_dir .. '/colorscheme.txt', 'w')
   if file then
@@ -19,13 +16,12 @@ function M.save_colorscheme(color_name)
   end
 end
 
-function M.load_cached_colorscheme()
+local function load_cached_colorscheme()
   local cache_dir = vim.fn.stdpath('cache')
   local file = io.open(cache_dir .. '/colorscheme.txt', 'r')
   if file then
     local color_name = file:read()
     file:close()
-    -- Safely try to set the colorscheme
     local status_ok = pcall(vim.cmd.colorscheme, color_name)
     if not status_ok then
       vim.notify('Failed to load colorscheme: ' .. color_name, vim.log.levels.WARN)
@@ -33,37 +29,45 @@ function M.load_cached_colorscheme()
   end
 end
 
+local function get_colorschemes()
+  return vim.fn.getcompletion("", "color")
+end
+
+local function preview_colorscheme(colorscheme_name)
+  vim.cmd.colorscheme(colorscheme_name)
+end
+
+local function pick_colorscheme(opts)
+  opts = opts or {}
+  local colorschemes = get_colorschemes()
+
+  vim.ui.select(colorschemes,
+    {
+      prompt = "Select a colorscheme:",
+      format_item = function(item)
+        return item
+      end,
+    },
+    function(choice)
+      if choice then
+        preview_colorscheme(choice)
+        save_colorscheme(choice)
+      end
+    end
+  )
+end
 
 M.setup = function(opts)
   opts = vim.tbl_deep_extend("force", defaults, opts or {})
 
-  vim.keymap.set("n", opts.keys, function()
-    local current_buf = vim.api.nvim_get_current_buf()
-
-    builtin.colorscheme({
-      attach_mappings = function(_, map)
-        map('i', '<CR>', function(prompt_bufnr)
-          local selection = require('telescope.actions.state').get_selected_entry()
-          require('telescope.actions').close(prompt_bufnr)
-          vim.cmd.colorscheme(selection.value)
-          M.save_colorscheme(selection.value)
-        end)
-        return true
-      end,
-      preview = {
-        buffer_previewer_maker = function()
-          return current_buf
-        end
-      },
-      previewer = true,
-      enable_preview = opts.preview
-    })
-  end, { silent = true, desc = "Colorscheme picker" })
+  if opts.keymap then
+    vim.keymap.set("n", opts.keymap, function() pick_colorscheme(opts) end)
+  end
 
   if colorscheme_overwrite then
     vim.cmd.colorscheme(colorscheme_overwrite)
   else
-    M.load_cached_colorscheme()
+    load_cached_colorscheme()
   end
 end
 
