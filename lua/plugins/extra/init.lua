f3018ccbaaaea240257fcc657193af30bcb8bb59
nixos <orisneh@gmail.com>
2024-12-03 14:56:03 +0000

    Added mini.nvim collection to extras

--- lua/plugins/extra/init.lua
+++ lua/plugins/extra/init.lua
@@ -1,1 +1,15 @@
-return {}
+return {
+
+  -- mini.nvim collection
+  {
+    "echasnovski/mini.nvim", version = "*", config = function()
+      -- text alignment plugin
+      require("mini.align").setup()
+      -- icon provider (requires patched font)
+      require("mini.icons").setup()
+      -- auto pair common characters
+      require("mini.pairs").setup()
+    end
+  },
+
+}
